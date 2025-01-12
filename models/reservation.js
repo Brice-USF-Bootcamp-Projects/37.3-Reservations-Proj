b7f2a8d0b3a485eb1d10a9fc662835f57f5490bc
Brice Nelson <bnelsonemail@icloud.com>
2025-01-12 00:09:06 -0500

    refactored reservation.js to save a reservation.

--- models/reservation.js
+++ models/reservation.js
@@ -1,14 +1,14 @@
 /** Reservation for Lunchly */
 
+/** Reservation for Lunchly */
+
 const moment = require("moment");
-
 const db = require("../db");
-
 
 /** A reservation for a party */
 
 class Reservation {
-  constructor({id, customerId, numGuests, startAt, notes}) {
+  constructor({ id, customerId, numGuests, startAt, notes }) {
     this.id = id;
     this.customerId = customerId;
     this.numGuests = numGuests;
@@ -18,27 +18,49 @@ class Reservation {
 
   /** formatter for startAt */
 
-  getformattedStartAt() {
-    return moment(this.startAt).format('MMMM Do YYYY, h:mm a');
+  get formattedStartAt() {
+    return moment(this.startAt).format("MMMM Do YYYY, h:mm a");
   }
 
   /** given a customer id, find their reservations. */
 
   static async getReservationsForCustomer(customerId) {
     const results = await db.query(
-          `SELECT id, 
+      `SELECT id, 
            customer_id AS "customerId", 
            num_guests AS "numGuests", 
            start_at AS "startAt", 
            notes AS "notes"
          FROM reservations 
          WHERE customer_id = $1`,
-        [customerId]
+      [customerId]
     );
 
     return results.rows.map(row => new Reservation(row));
   }
-}
 
+  /** save this reservation. */
+
+  async save() {
+    if (this.id === undefined) {
+      // Insert a new reservation
+      const result = await db.query(
+        `INSERT INTO reservations (customer_id, num_guests, start_at, notes)
+             VALUES ($1, $2, $3, $4)
+             RETURNING id`,
+        [this.customerId, this.numGuests, this.startAt, this.notes]
+      );
+      this.id = result.rows[0].id;
+    } else {
+      // Update an existing reservation
+      await db.query(
+        `UPDATE reservations 
+             SET customer_id=$1, num_guests=$2, start_at=$3, notes=$4
+             WHERE id=$5`,
+        [this.customerId, this.numGuests, this.startAt, this.notes, this.id]
+      );
+    }
+  }
+}
 
 module.exports = Reservation;
